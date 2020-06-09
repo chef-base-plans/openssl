e9f4c560c2b24560ea00be6908ad1ab01ed1d5d8
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-09 12:12:10 +0100

    Add build status badge and update tests according to agreed pattern
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/openssl_exists.rb
+++ controls/openssl_exists.rb
@@ -1,17 +1,23 @@
 title 'Tests to confirm openssl exists'
 
+plan_origin = ENV['HAB_ORIGIN']
 plan_name = input('plan_name', value: 'openssl')
-plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
-openssl_relative_path = input('command_path', value: '/bin/openssl')
-openssl_installation_directory = command("hab pkg path #{plan_ident}")
-openssl_full_path = openssl_installation_directory.stdout.strip + "#{ openssl_relative_path}"
- 
+
 control 'core-plans-openssl-exists' do
   impact 1.0
   title 'Ensure openssl exists'
   desc '
-  '
-   describe file(openssl_full_path) do
+  Verify openssl by ensuring /bin/openssl exists'
+  
+  plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
+  describe plan_installation_directory do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+
+  command_relative_path = input('command_relative_path', value: '/bin/openssl')
+  command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
+  describe file(command_full_path) do
     it { should exist }
   end
 end
