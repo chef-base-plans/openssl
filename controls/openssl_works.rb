65e8dd92009e7c4635a8014923ad8b90afee6dcf
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-16 17:32:42 +0100

    Update documentation
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/openssl_works.rb
+++ controls/openssl_works.rb
@@ -7,26 +7,30 @@ control 'core-plans-openssl-works' do
   impact 1.0
   title 'Ensure openssl works as expected'
   desc '
-  Verify openssl by ensuring (1) its installation directory exists and (2) that
-  it returns the expected version; (3) it successfully base64 encodes text
+  Verify openssl by ensuring that
+  (1) its installation directory exists 
+  (2) it returns the expected version
+  (3) it successfully base64 encodes text
   '
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
   end
   
-  plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
-  plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
-  describe command("DEBUG=true; hab pkg exec #{plan_pkg_ident} openssl version") do
+  command_relative_path = input('command_relative_path', value: 'bin/openssl')
+  command_full_path = File.join(plan_installation_directory.stdout.strip, command_relative_path)
+  plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
+  describe command("#{command_full_path} version") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
     its('stdout') { should match /OpenSSL #{plan_pkg_version}/ }
     its('stderr') { should be_empty }
   end
 
-  describe command("echo 'a_byte_character' | hab pkg exec #{plan_pkg_ident} openssl enc -base64") do
+  describe command("echo 'a_byte_character' | #{command_full_path} enc -base64") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
     its('stdout') { should match /YV9ieXRlX2NoYXJhY3Rlcgo=/ }
