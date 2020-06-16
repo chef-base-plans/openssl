8e7480153c0fd8b6b4aa9337cf92efd1c6848c0b
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-16 17:32:42 +0100

    Update documentation
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/openssl_exists.rb
+++ controls/openssl_exists.rb
@@ -7,16 +7,17 @@ control 'core-plans-openssl-exists' do
   impact 1.0
   title 'Ensure openssl exists'
   desc '
-  Verify openssl by ensuring /bin/openssl exists'
+  Verify openssl by ensuring bin/openssl exists'
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
   end
 
-  command_relative_path = input('command_relative_path', value: '/bin/openssl')
-  command_full_path = plan_installation_directory.stdout.strip + "#{command_relative_path}"
+  command_relative_path = input('command_relative_path', value: 'bin/openssl')
+  command_full_path = File.join(plan_installation_directory.stdout.strip, command_relative_path)
   describe file(command_full_path) do
     it { should exist }
   end
